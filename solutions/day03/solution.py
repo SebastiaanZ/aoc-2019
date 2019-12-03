31575d1ae6c95ae267a920616e9cea89432fc6a6
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2019-12-03 12:51:59 +0100

    Add solution for day 3 of Advent of Code
    I have added my solution for day 3 of the Advent of Code. While this
    was not my initial solution (that one used numpy), this is a solution
    that avoids creating all the intermediate points for the different
    line segments of the wires.

---
+++ solutions/day03/solution.py
@@ -0,0 +1,119 @@
+from __future__ import annotations
+
+import collections
+import enum
+from typing import List, NamedTuple, Optional, Tuple
+
+
+class Orientation(enum.IntEnum):
+    VERTICAL = 0
+    HORIZONTAL = 1
+
+
+Move = collections.namedtuple("Move", ("direction", "distance"))
+Intersection = collections.namedtuple("Intersection", "location steps_wire_one steps_wire_two")
+
+
+class Point(NamedTuple):
+    x: int
+    y: int
+
+    def __sub__(self, other: Point) -> Point:
+        """Subtract `other` from `self`."""
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __matmul__(self, other: Point) -> int:
+        """Calculate the cityblock distance between `self` and `other`."""
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def __eq__(self, other: Point) -> bool:
+        """Compare if `self` is equal to `other` by comparing coordinates."""
+        return self.x == other.x and self.y == other.y
+
+
+class Line:
+    def __init__(self, start: Point, end: Point, start_steps: int):
+        self.start = start
+        self.end = end
+        self.orientation = Orientation.HORIZONTAL if start.y == end.y else Orientation.VERTICAL
+        self.start_steps = start_steps
+
+    def intersection(self, other: Line) -> Optional[Intersection]:
+        """Return the intersection with `other` if it exists; otherwiser return `None`."""
+        if self.orientation == other.orientation:
+            return
+
+        start_diff = self.start - other.start
+        end_diff = self.end - other.end
+
+        if start_diff.x * end_diff.x <= 0 and start_diff.y * end_diff.y <= 0:
+            if self.orientation == Orientation.VERTICAL:
+                location = Point(self.start.x, other.start.y)
+            else:
+                location = Point(other.start.x, self.start.y)
+
+            return Intersection(
+                location,
+                self.start_steps + (self.start @ location),
+                other.start_steps + (other.start @ location),
+            )
+
+    def __repr__(self) -> str:
+        """Create internal representation of the object."""
+        return f"Line(start={self.start}, end={self.end}, start_steps={self.start_steps}"
+
+
+def draw_lines(wire: List[str]) -> List[Line]:
+    """Create the line segments that make up a wire."""
+    move_point = {
+        "U": lambda start, dist: Point(start.x, start.y + dist),
+        "R": lambda start, dist: Point(start.x + dist, start.y),
+        "D": lambda start, dist: Point(start.x, start.y - dist),
+        "L": lambda start, dist: Point(start.x - dist, start.y),
+    }
+
+    moves = [Move(direction, int(''.join(distance))) for direction, *distance in wire.split(',')]
+
+    location = Point(0, 0)
+    steps = 0
+    lines = []
+    for move in moves:
+        new_location = move_point[move.direction](location, move.distance)
+        lines.append(Line(location, new_location, steps))
+        steps += move.distance
+        location = new_location
+
+    return lines
+
+
+def find_intersections(data: List[List[Line]]) -> List[Intersection]:
+    """Find the intersections between line segments in both lists."""
+    wire_one, wire_two = data
+    origin = Point(0, 0)
+    intersections = [
+        intersection for a in wire_one for b in wire_two
+        if (intersection := a.intersection(b)) and intersection.location != origin  # noqa
+    ]
+    return intersections
+
+
+def part_one(intersections: List[Intersection]) -> int:
+    """Find the intersection closest the origin in terms of the Manhattan distance."""
+    origin = Point(0, 0)
+    return min(i.location @ origin for i in intersections)
+
+
+def part_two(intersections: List[Intersection]) -> int:
+    """Find the intersection closest to the origin in terms of the number of steps taken."""
+    return min(i.steps_wire_one + i.steps_wire_two for i in intersections)
+
+
+def main(data: List[str]) -> Tuple[int]:
+    """The main function taking care of parsing the input data and running the solutions."""
+    data = [draw_lines(wire) for wire in data]
+    intersections = find_intersections(data)
+
+    answer_one = part_one(intersections)
+    answer_two = part_two(intersections)
+
+    return answer_one, answer_two
