999c09e4a65d86d084fcb776b826cbd0b861f020
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2019-12-06 09:27:14 +0100

    Include faster and non-NetworkX solution for day 6
    This commits replaces the `NetworkX` solution by a solution that only
    the standard library. Since it's optimized for task, it's is more
    than twice as fast as the NetworkX solution.
    
    The NetworkX solution has been renamed to `solution_networkx.py`.

--- solutions/day06/solution.py
+++ solutions/day06/solution.py
@@ -1,22 +1,94 @@
-from typing import List, Tuple
+from __future__ import annotations
 
-import networkx as nx
+from operator import attrgetter
+from typing import Iterator, List, Tuple
 
 
-def part_one(orbits) -> int:
+class Node:
+    """An orbital node used the OrbitalMap."""
+
+    __slots__ = ["name", "parent", "_depth", "_ancestors"]
+
+    def __init__(self, name: str):
+        self.name = name
+        self.parent = None
+        self._depth = None
+        self._ancestors = None
+
+    def __repr__(self) -> str:
+        """Return a developer-friendly representation of the node."""
+        cls = self.__class__
+        return f"{cls.name}(name={self.name!r})"
+
+    def __hash__(self) -> int:
+        """Hash the node using its name."""
+        return hash(self.name)
+
+    @property
+    def depth(self) -> int:
+        """Calculate the distance from this node to the Central Orbital Mass."""
+        if self._depth is None:
+            if self.parent:
+                self._depth = self.parent.depth + 1
+            else:
+                self._depth = 0
+
+        return self._depth
+
+    @property
+    def ancestors(self) -> List[Node]:
+        """List all the ancestors of the node."""
+        if self._ancestors is None:
+            self._ancestors = []
+            node = self
+            while (node := node.parent):  # noqa: E203, E231
+                self._ancestors.append(node)
+        return self._ancestors
+
+
+class OrbitalMap:
+    """A map of all known orbits, including my orbit and Santa's orbit."""
+
+    def __init__(self):
+        self.nodes = {}
+
+    def __iter__(self) -> Iterator[Node]:
+        """Return an iterator over all orbital nodes in the map."""
+        return iter(self.nodes.values())
+
+    def __getitem__(self, key: str) -> Node:
+        """Get a specific node from the orbital map using its name."""
+        return self.nodes[key]
+
+    def add_orbit(self, parent, child):
+        """Register an orbit of `child` around `parent`, creating the nodes if they are new."""
+        parent = self.nodes.setdefault(parent, Node(name=parent))
+        child = self.nodes.setdefault(child, Node(name=parent))
+        child.parent = parent
+
+
+def part_one(orbital_map) -> int:
     """Compute the total number of direct and indirect orbits in the orbital map."""
-    return sum(nx.shortest_path_length(orbits, source="COM").values())
+    return sum(node.depth for node in orbital_map)
 
 
-def part_two(orbits) -> int:
+def part_two(orbital_map) -> int:
     """Compute the number of orbital transfers needed to get to Santa."""
-    return nx.shortest_path_length(orbits, source="YOU", target="SAN") - 2
+    me = orbital_map["YOU"]
+    santa = orbital_map["SAN"]
+    common_ancestor = max(
+        set(me.ancestors).intersection(santa.ancestors),
+        key=attrgetter("depth")
+    )
+    return (me.depth - common_ancestor.depth) + (santa.depth - common_ancestor.depth) - 2
 
 
 def main(data: List[str]) -> Tuple[int]:
     """Run my solution to day 6 of the Advent of Code."""
-    orbits = nx.Graph()
-    orbits.add_edges_from([number.split(")") for number in data])
-    answer_one = part_one(orbits)
-    answer_two = part_two(orbits)
+    orbital_map = OrbitalMap()
+    for orbit in data:
+        orbital_map.add_orbit(*orbit.split(")"))
+
+    answer_one = part_one(orbital_map)
+    answer_two = part_two(orbital_map)
     return answer_one, answer_two
